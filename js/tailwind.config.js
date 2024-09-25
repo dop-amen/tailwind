72588ec3058e4b10ffc0c6e674589a9e0fe42835
Aminul Islam <aminulabir2004@gmail.com>
2024-09-25 23:25:49 +0600

    Update tailwind.config.js

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -3,7 +3,7 @@ tailwind.config = {
       extend: {
           // background image start
           backgroundImage: {
-            'banner': "url('../images/bannerbg.png')",
+            'banner': "url('../images/bannerbg.jpg')",
           },
           // background image end
         colors: {
