7ded71f866acc3c4e4f483d86b2b71ffb83304dd
Aminul Islam <aminulabir2004@gmail.com>
2024-09-25 22:51:53 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,31 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          'navbarbg': 'rgba(22, 12, 109, 0.5)',
+          'hColor': '#1BBF00',
+          'over':'rgba(106, 98, 197, 0.9)'
+        },
+        maxWidth: {
+            'HeaderContainer': '1320px',
+          },
+          maxWidth: {
+            'bannerContainer': '950px',
+          },
+          transitionDuration: {
+            '200': '200ms',
+          },
+          backgroundImage: {
+            'banner': "url('../images/bannerbg.jpg')",
+          },
+          fontFamily: {
+            'open': ["Open Sans"],
+            
+          },
+          fontFamily: {
+            'pap': ["Paprika"],
+            
+          }
+      }
+    }
+  }
